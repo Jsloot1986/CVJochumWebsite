5cb76d431bcfd2c4ebe226ca4dc5b454c4982258
Jochum Sloot <67738459+Jsloot1986@users.noreply.github.com>
2021-04-16 15:30:26 +0200

    update CV website
    Put some new project in my cv

---
+++ projectCV/jscv.js
@@ -0,0 +1,24 @@
+function emailHover(){
+    document.querySelector('.email').style.fontSize = '300%';
+}
+function emailNormal(){
+    document.querySelector('.email').style.fontSize = '100%';
+}
+function facebookHover(){
+    document.querySelector('.facebook').style.fontSize = '300%';
+}
+function facebookNormal(){
+    document.querySelector('.facebook').style.fontSize = '100%';
+}
+function instagramHover(){
+    document.querySelector('.instagram').style.fontSize = '300%';
+}
+function instagramNormal(){
+    document.querySelector('.instagram').style.fontSize = '100%';
+}
+function linkedinHover(){
+    document.querySelector('.linkedin').style.fontSize = '300%';
+}
+function linkedinNormal(){
+    document.querySelector('.linkedin').style.fontSize = '100%';
+}
